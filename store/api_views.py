ed62bc723cedc97fdcabc1ea35928fa32498f05e
AEsir777 <lkb2438834817@gmail.com>
2022-11-01 00:11:10 -0400

    create view

--- store/api_views.py
+++ store/api_views.py
@@ -1,14 +1,25 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 
 from store.serializers import ProductSerializer
 from store.models import Product
 
+class ProductsPagination(LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 1000
+
 class ProductListAPIView(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['id']
+    # enable full text search
+    search_fields = ['name', 'description']
+    # enable pagination
+    pagination_class = ProductsPagination
 
     # filter with more complex field
     def get_queryset(self):
@@ -23,3 +34,20 @@ class ProductListAPIView(ListAPIView):
                 sale_end__gte=now,
             )
         return self.queryset
+
+class ProductCreateAPIView(CreateAPIView):
+    serializer_class = ProductSerializer
+
+    def create(self, request, *args, **kwargs):
+        try:
+            price = request.data.get('price')
+            if price is not None and float(price) <= 0.0:
+                raise ValidationError({ 'price': 'Must be positive' })
+        except ValueError:
+            raise ValidationError({ 'price': 'A valid number is required' })
+        return super().create(request, *args, **kwargs)
+
+
+""" class ProductDestroyAPIView(DestroyAPIView):
+    serializer_class = Pr
+ """
--- store/urls.py
+++ store/urls.py
@@ -7,7 +7,7 @@ import store.api_views
 
 urlpatterns = [
     path('api/example1', store.api_views.ProductListAPIView.as_view(), name='api-list'),
-
+    path('api/example1/new', store.api_views.ProductCreateAPIView.as_view(), name='api-create'),
     path('products/<int:id>/', store.views.show, name='show-product'),
     path('cart/', store.views.cart, name='shopping-cart'),
     path('', store.views.index, name='list-products'),
